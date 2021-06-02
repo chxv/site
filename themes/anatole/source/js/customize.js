e0200216d2d6cbb6b709f360c0f2e061fe156621
xander <1149277941@qq.com>
2021-06-02 11:26:03 +0800

    fix: set remark_config global

--- themes/anatole/source/js/customize.js
+++ themes/anatole/source/js/customize.js
@@ -23,16 +23,17 @@ function getCookie(cname) {
   return "";
 }
 
+// 必须是全局变量
+var remark_config = {
+  host: "https://api.xchens.cn",
+  site_id: 'xchens_cn',
+  components: ['embed']
+};
 function showComment() {
   if (document.getElementById("remark42") == null) {
     console.log("not found comment div, stop show comment");
     return;
   }
-  var remark_config = {
-    host: "https://api.xchens.cn",
-    site_id: 'xchens_cn',
-    components: ['embed']
-  };
   !function (e, n) { for (var o = 0; o < e.length; o++) { var r = n.createElement("script"), c = ".js", d = n.head || n.body; "noModule" in r ? (r.type = "module", c = ".mjs") : r.async = !0, r.defer = !0, r.src = remark_config.host + "/web/" + e[o] + c, d.appendChild(r) } }(remark_config.components || ["embed"], document);
 }
 
